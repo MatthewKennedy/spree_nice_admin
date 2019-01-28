9522adb700c29fda4d8268a95e40f69422d84a89
Matthew Kennedy <m.kennedy@me.com>
2019-01-28 17:09:50 +0000

    Initial commit

---
+++ app/assets/javascripts/spree/backend/nice_ui.js
@@ -0,0 +1,29 @@
+
+
+jQuery(function ($) {
+  var wrapper = $('#wrapper')
+  var main = $('#main-part')
+  var sidebar = $('#main-sidebar')
+
+  var mainWrapperCollapsedClasses = 'main col-xs-12 sidebar-collapsed'
+  var mainWrapperExpandedClasses = 'main col-xs-9 col-xs-offset-3 col-md-10 col-md-offset-2'
+
+  sidebar.removeClass('hidden-xs col-xs-3 col-md-2 sidebar')
+  wrapper.removeClass('sidebar-minimized')
+  main
+    .removeClass(mainWrapperCollapsedClasses)
+    .removeClass(mainWrapperExpandedClasses)
+
+  // Sidebar nav toggle functionality
+  $('#sidebar-toggle').on('click', function () {
+
+    wrapper.toggleClass('sidebar-opened')
+    sidebar.toggleClass('hidden-xs')
+    wrapper.removeClass('sidebar-minimized')
+    main
+      .removeClass(mainWrapperCollapsedClasses)
+      .removeClass(mainWrapperExpandedClasses)
+  })
+
+
+})
---
+++ lib/spree_nice_admin.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_nice_admin/engine'
+require 'spree_nice_admin/version'
