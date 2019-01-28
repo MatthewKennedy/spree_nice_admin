9e25e4107b5f8d6889bdcfb1aeb901983f334ec7
Matthew Kennedy <m.kennedy@me.com>
2019-01-28 19:55:33 +0000

    add some niceness

--- app/assets/javascripts/spree/backend/spree_nice_admin.js
+++ app/assets/javascripts/spree/backend/spree_nice_admin.js
@@ -1,5 +1,3 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
-
-//= require spree/backend/scroll_lock
 //= require spree/backend/nice_ui
