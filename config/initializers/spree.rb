77d6f8c6f1477f5d976deafeb1602b75f9325650
Matthew Kennedy <m.kennedy@me.com>
2019-01-29 09:46:57 +0000

    improve ui

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,3 +1,3 @@
 Spree.config do |config|
-  config.admin_interface_logo = "https://spree-guides.now.sh/static/a49312f48772382ea53e3f79f4e18609/850af/logo.png"
+  config.admin_interface_logo = config.logo
 end
